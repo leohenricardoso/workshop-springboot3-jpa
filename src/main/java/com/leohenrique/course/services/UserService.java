471fd6709af1e1c9f8437dce83ad63ea0ec75300
Leonardo Henrique <leohenricardoso@gmail.com>
2024-10-28 17:58:42 -0300

    Exception handler - User findById

--- src/main/java/com/leohenrique/course/services/UserService.java
+++ src/main/java/com/leohenrique/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.leohenrique.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.leohenrique.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = this.repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User user) {
